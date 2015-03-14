566a4143df40cb3f292bf6a39e578d49fbb42225
Robert Trzebinski <robert@trzebinski.info>
2015-03-14 19:57:29 +0000

    remove unused setupLayout method

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -8,17 +8,4 @@ class BaseController extends Controller {
 	 */
 	protected $viewData = array();
 
-	/**
-	 * Setup the layout used by the controller.
-	 *
-	 * @return void
-	 */
-	protected function setupLayout()
-	{
-		if ( ! is_null($this->layout))
-		{
-			$this->layout = View::make($this->layout);
-		}
-	}
-
 }
