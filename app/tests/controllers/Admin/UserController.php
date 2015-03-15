4800d013fe5dbc222be9d859e4a32bd059b251fd
Robert Trzebinski <robert@trzebinski.info>
2015-03-15 13:44:31 +0000

    unit tests for Admin/UserController

--- app/tests/controllers/Admin/UserController.php
+++ app/tests/controllers/Admin/UserController.php
@@ -1,21 +1,68 @@
 <?php
 
-class UserControllerTest extends TestCase {
+class Tests_Controllers_Admin_UserController extends TestCase {
 
-	public function testGetOverview() {
-		
+	public function testGetOverview_user_logged_in()
+	{
+		$user = $this->createUser();
+		$this->be($user);
+
+		$this->route('GET', 'admin_overview');
+
+		$this->assertViewHas('user');
 	}
 
-	public function testGetLogin() {
-		
+	public function testGetOverview_user_not_logged_in()
+	{
+		$this->route('GET', 'admin_overview');
+
+		$this->assertRedirectedToRoute('admin_user_login');
 	}
 
-	public function testPostLogin() {
-		
+	public function testGetLogin()
+	{
+		$this->route('GET', 'admin_user_login');
+
+		$this->assertResponseOk();
 	}
 
-	public function testGetLogout() {
+	public function testPostLogin_ok()
+	{
+		$user = $this->createUser();
+		$data = [
+			'email' => $user->email,
+			'password' => $user->email
+		];
+
+		$this->route('POST', 'admin_user_login', $data);
+
+		$this->assertRedirectedToRoute('admin_overview');
+		$this->assertTrue(Auth::check());
+	}
+
+	public function testPostLogin_fail()
+	{
+		$user = $this->createUser();
+		$data = [
+			'email' => $user->email,
+			'password' => uniqid()
+		];
+
+		$this->route('POST', 'admin_user_login', $data);
+
+		$this->assertViewHas('errors');
+		$this->assertFalse(Auth::check());
+	}
+
+	public function testGetLogout()
+	{
+		$user = $this->createUser();
+		$this->be($user);
+
+		$this->route('GET', 'admin_user_logout');
 		
+		$this->assertRedirectedToRoute('admin_user_login');
+		$this->assertFalse(Auth::check());
 	}
 
 }
