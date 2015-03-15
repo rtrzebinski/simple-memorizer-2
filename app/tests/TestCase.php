3f229e86089bd53fbd677cb80b1fde19c3b3a7c7
Robert Trzebinski <robert@trzebinski.info>
2015-03-15 13:43:30 +0000

    add some unit tests helpers

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -13,7 +13,25 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 		$testEnvironment = 'testing';
 
-		return require __DIR__.'/../../bootstrap/start.php';
+		return require __DIR__ . '/../../bootstrap/start.php';
+	}
+
+	/**
+	 * @return User
+	 */
+	protected function createUser()
+	{
+		$user = App::make('User');
+		$user->email = uniqid() . '@blackhole.io';
+		$user->password = Hash::make($user->email);
+		$user->name = 'test';
+		$user->save();
+		return $user;
+	}
+
+	protected function dumpResponseContent()
+	{
+		dd($this->client->getResponse()->getContent());
 	}
 
 }
