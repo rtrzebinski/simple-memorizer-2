1cfb589c396d097b0803865771b0b089fbf01b07
Robert Trzebinski <robert@trzebinski.info>
2015-05-02 11:53:34 +0100

    add jtable

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -1,5 +1,9 @@
 <html>
 	<head>
+		<!-- Include one of jTable styles. -->
+		<link href="jtable.2.4.0/themes/metro/blue/jtable.min.css" rel="stylesheet" type="text/css" />
+		<!-- Include jTable script file. -->
+		<script src="jtable.2.4.0/jquery.jtable.min.js" type="text/javascript"></script>
 		@yield('head')
 	</head>
     <body>
