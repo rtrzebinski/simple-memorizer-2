8071e7614abd192afdf93c59546df33cb51053f8
Robert Trzebinski <robert@trzebinski.info>
2015-05-04 12:39:41 +0100

    add sample questions and answers to dev seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -8,7 +8,7 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run() {
 		Eloquent::unguard();
-		$this->call('UserTableSeeder');
+		$this->call('SampleDataSeeder');
 	}
 
 }
--- app/database/seeds/SampleDataSeeder.php
+++ app/database/seeds/SampleDataSeeder.php
@@ -1,16 +1,54 @@
 <?php
 
-class UserTableSeeder extends Seeder {
+class SampleDataSeeder extends Seeder {
 
-	public function run() {
-		// delete existing db rows
-		DB::table('users')->delete();
-		// create new user
-		User::create(array(
-			'name' => 'test user 1',
-			'email' => 'foo@bar.com',
-			'password' => Hash::make('password')
-		));
+	public function run()
+	{
+		$user = User::create([
+				'name' => 'test user 1',
+				'email' => 'foo@bar.com',
+				'password' => Hash::make('password')
+		]);
+		$question1 = Question::create([
+				'question' => '1 + 1',
+				'answer' => '2'
+		]);
+		$question2 = Question::create([
+				'question' => '2 + 2',
+				'answer' => '4'
+		]);
+		$question3 = Question::create([
+				'question' => '3 + 3',
+				'answer' => '6'
+		]);
+		$question4 = Question::create([
+				'question' => '4 + 4',
+				'answer' => '8'
+		]);
+		$question5 = Question::create([
+				'question' => '5 + 5',
+				'answer' => '10'
+		]);
+		User_Question::create([
+			'user_id' => $user->id,
+			'question_id' => $question1->id
+		]);
+		User_Question::create([
+			'user_id' => $user->id,
+			'question_id' => $question2->id
+		]);
+		User_Question::create([
+			'user_id' => $user->id,
+			'question_id' => $question3->id
+		]);
+		User_Question::create([
+			'user_id' => $user->id,
+			'question_id' => $question4->id
+		]);
+		User_Question::create([
+			'user_id' => $user->id,
+			'question_id' => $question5->id
+		]);
 	}
 
 }
