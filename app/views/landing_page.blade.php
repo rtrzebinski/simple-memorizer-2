9e7185558f1089dbf90d6ec2802ce76a0566de76
Robert Trzebinski <robert@trzebinski.info>
2015-08-23 19:02:36 +0100

    style landing page

--- app/views/landing_page.blade.php
+++ app/views/landing_page.blade.php
@@ -1,7 +1,49 @@
 @extends('layouts.main')
 
 @section('content')
-	<h3>this is LANDING page</h3>
-	<h3><a href="{{{ route('login') }}}">login</a></h3>
-	<h3><a href="{{{ route('signup') }}}">signup</a></h3>
+
+<div class="container">
+
+	<div class="header clearfix">
+        <nav>
+			<ul class="nav nav-pills pull-right">
+				<li role="presentation" class="active"><a href="/">Home</a></li>
+				<li role="presentation"><a href="{{{ route('login') }}}">Login</a></li>
+			</ul>
+        </nav>
+        <h3 class="text-muted">Simple Memorizer 2</h3>
+	</div>
+
+	<div class="jumbotron">
+        <h2>Memorizing does not have to be hard</h2>
+        <p class="lead">Simply learn anything you want. Efficiently practice recalling of you knowledge.</p>
+        <p><a class="btn btn-lg btn-success" href="{{{ route('signup') }}}" role="button">Sign up for free</a></p>
+	</div>
+
+	<div class="row marketing">
+        <div class="col-lg-6">
+			<h4>It's easier than you think</h4>
+			<p>Start in just few minutes, we keep things as simple as possible</p>		
+
+			<h4>The floor is yours</h4>
+			<p>Build your own knowledge database, or import an existing one</p>
+
+			<h4>Learn efficiently</h4>
+			<p>No time to lose, memorizing by recalling is more effective than just reading</p>
+        </div>
+
+        <div class="col-lg-6">
+			<h4>It's absolutely free</h4>
+			<p>You will never have to pay for using this app</p>
+
+			<h4>Take advantage of every minute</h4>
+			<p>Waiting for the bus? Spend few minutes for recalling your knowledge</p>
+
+			<h4>Access everywhere</h4>
+			<p>Run on your computer, table or even phone</p>
+        </div>
+	</div>
+
+</div>
+
 @stop
