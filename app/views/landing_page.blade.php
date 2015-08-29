944c64c2fc42f80d34af54f93e30dfcfc1bac0a2
Robert Trzebinski <robert@trzebinski.info>
2015-08-29 23:14:20 +0100

    remove unused 'home' button

--- app/views/landing_page.blade.php
+++ app/views/landing_page.blade.php
@@ -7,7 +7,6 @@
 	<div class="header clearfix">
         <nav>
 			<ul class="nav nav-pills pull-right">
-				<li role="presentation" class="active"><a href="/">Home</a></li>
 				<li role="presentation"><a href="{{{ route('login') }}}">Login</a></li>
 			</ul>
         </nav>
