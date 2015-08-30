db415ce4d3072cdc359d9bd64d6ded43c94bb60f
Robert Trzebinski <robert@trzebinski.info>
2015-08-30 11:44:26 +0100

    map keyboard keys to buttons in learning page

--- app/views/learning_page.blade.php
+++ app/views/learning_page.blade.php
@@ -4,15 +4,43 @@
 <script type='text/javascript'>
 	$(document).ready(function() {
 		// when 'show answer' button clicked
-		$('#show_answer').click(function(event) {
+		$('#show_answer_button').click(function(event) {
 			event.preventDefault();
 			// hide 'show answer' button
-			$('#show_answer').hide();
+			$('#show_answer_button').hide();
 			// display answer
 			$('#answer_div').removeClass('hidden');
 			// set 'display_answer' HTTP parameter to true, so answer will be visible after page reloading
 			$('#display_answer').val(true);
 		});
+		// map keyboard keys to buttons
+		$("body").keyup(function(ev) {
+			// ignore when text is being written
+			if ($(ev.target).is('input, textarea, select')) {
+				return;
+			}
+			// show answer on space click
+			if (ev.which == 32) {
+				// 32 = space
+				$("#show_answer_button").click();
+			}
+			// mark answer as good on 'g' key click
+			if (ev.which == 71) {
+				// 71 = g
+				$("#good_answer_button").click();
+			}
+			// mark answer as bad on 'b' key click
+			if (ev.which == 66) {
+				// 66 = b
+				$("#bad_answer_button").click();
+			}
+			// show next question on 'b' key click
+			if (ev.which == 78) {
+				// 78 = n
+				$("#next_question_button").click();
+			}
+		});
+		F
 	});
 </script>
 @stop
@@ -26,10 +54,10 @@
 
 	<label for="actions">Actions</label>
 	<div id="actions">
-		<input class="btn btn-default btn-success btn-lg" type="submit" value="Good" name="answer_correctness">&nbsp;&nbsp;&nbsp;
-		<input class="btn btn-default btn-danger btn-lg" type="submit" value="Bad" name="answer_correctness">&nbsp;&nbsp;&nbsp;
-		<input class="btn btn-default btn-lg" type="submit" value="Next">&nbsp;&nbsp;&nbsp;
-		<button id="show_answer" class="btn btn-default btn-lg @if($display_answer) hidden @endif">Show answer</button>&nbsp;&nbsp;&nbsp;
+		<input id='good_answer_button' class="btn btn-default btn-success btn-lg" type="submit" value="Good" name="answer_correctness">&nbsp;&nbsp;&nbsp;
+		<input id='bad_answer_button' class="btn btn-default btn-danger btn-lg" type="submit" value="Bad" name="answer_correctness">&nbsp;&nbsp;&nbsp;
+		<input id='next_question_button' class="btn btn-default btn-lg" type="submit" value="Next">&nbsp;&nbsp;&nbsp;
+		<button id='show_answer_button' class="btn btn-default btn-lg @if($display_answer) hidden @endif">Show answer</button>&nbsp;&nbsp;&nbsp;
 		<input type="hidden" name="user_question_id" value="{{{ $user_question_id }}}">
 		<input type="hidden" name="display_answer" id="display_answer" value="{{{ $display_answer }}}">
 	</div>
